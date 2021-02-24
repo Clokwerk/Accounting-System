656c8df9bac39e23d783722c84aa5df1d3d118e0
Clokwerk <35271524+Clokwerk@users.noreply.github.com>
2021-02-24 14:12:15 -0800

    Base code for Spring Security added
    Added base code for Spring Security: basic configuration file, custom authentication provider and necessary functionalities in the service layer. Also added one small test login page, along with a controller called AuthenticationController, which will serve as the central authentication endpoint.
    NOTE: Spring Security heavily conflicts with H2. Must transfer work to functional database as soon as possible.

---
+++ main/main/src/main/java/accountingsystem/main/model/User.java
@@ -0,0 +1,113 @@
+package accountingsystem.main.model;
+
+import jdk.jfr.DataAmount;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Table(name="users")
+public class User implements UserDetails {
+
+    @Id
+    private Long Id;
+    private String username;
+
+    private String password;
+
+    private String FirstName;
+
+    private String LastName;
+
+    private boolean isAccountNonExpired = true;
+    private boolean isAccountNonLocked = true;
+    private boolean isCredentialsNonExpired = true;
+    private boolean isEnabled = true;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+
+
+    public User(){
+
+}
+
+    public User(Long id, String username, String password, String firstName, String lastName) {
+        Id = id;
+        this.username = username;
+        this.password = password;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(role);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+}
